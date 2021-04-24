bc10c53433615631f42c12d69190f63847f23699
Tamer Karalürt <tamerkaralurt@toros.edu.tr>
2021-04-24 23:11:11 +0300

    düzenleme

--- telegram.php
+++ telegram.php
@@ -87,7 +87,7 @@ if (isset($data->text)) {
     switch (trim($data->text, '/')) {
         case 'karzarar':
             $dotusd = $telegram->getDotUsd();
-            $telegram->sendMessage("KAR ZARAR: Günlük 2 Saatlik: ".$dotusd->data->daily." Günlük 2 Saatlik Yüzde: % ".$dotusd->data->dailyPercent."");
+            $telegram->sendMessage("KAR ZARAR: Günlük 2 Saatlik: ".$dotusd['data']['daily']." Günlük 2 Saatlik Yüzde: % ".$dotusd['data']['dailyPercent']."");
             break;
         case 'durum':
             $telegram->sendMessage("DURUM");
